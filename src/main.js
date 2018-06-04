965d618bd24535adb45b0a9e2d462d0c3dd53bf7
xushuo <893804934@qq.com>
2018-06-04 18:37:02 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'css/reset.css'
+import 'css/border.css'
+import 'css/iconfont.css'
+import fastclick from 'fastclick'
+
+Vue.config.productionTip = false
+fastclick.attach(document.body) //取消点击延迟
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <home-hearder></home-hearder>
+  </div>
+
+</template>
+<script>
+  import homeHearder from './components/Header'
+  export default {
+    name : 'Home',
+    components : {
+      homeHearder
+    }
+  }
+</script>
+<style>
+
+</style>
