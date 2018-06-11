51840880f1e25fdb96cfa68b32f92dbe78d729f3
xushuo <xushuo@fclassroom.com>
2018-06-11 16:53:09 +0800

    城市选择  字母表

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,143 @@
+<template>
+    <div class="list" ref="wrapper">
+        <div>
+            <div class="area">
+                <div class="title border-topbottom">
+                    当前城市
+                </div>
+                <div class="button-list">
+                    <div class="button-wrapperr">
+                        <div class="button">北京</div>
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">
+                    热门城市
+                </div>
+                <div class="button-list">
+                    <div class="button-wrapperr">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapperr">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapperr">
+                        <div class="button">北京</div>
+                    </div>
+                    <div class="button-wrapperr">
+                        <div class="button">北京</div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="area border-topbottom">
+                <div class="title">
+                    A
+                </div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+            </div>
+            <div class="area border-topbottom">
+                <div class="title">
+                    A
+                </div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+            </div>
+            <div class="area border-topbottom">
+                <div class="title">
+                    A
+                </div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+            </div>
+            <div class="area border-topbottom">
+                <div class="title">
+                    A
+                </div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                    <div class="item border-bottom">阿拉尔</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import BScroll from 'better-scroll'
+    export default {
+        name : 'CityList',
+        mounted (){
+            this.scroll = new BScroll(this.$refs.wrapper)
+        }
+    }
+</script>
+<style scoped lang="scss" type="text/scss">
+    @import "~css/theme.scss";
+    .list{
+        position:absolute;
+        overflow:hidden;
+        top:1.58rem;
+        left:0;
+        right:0;
+        bottom:0;
+        .border-topbottom:before{
+            border-color:#777;
+        }
+        .border-topbottom:after{
+            border-color:#777;
+        }
+        .border-bottom:before{
+            border-color:#777;
+        }
+        .area{
+            .title{
+                line-height:.54rem;
+                background:#eee;
+                padding-left:.2rem;
+                color:#666;
+                font-size:.26rem;
+            }
+            .button-list{
+                overflow:hidden;
+                padding:.1rem .6rem .1rem .1rem;
+                .button-wrapperr{
+                    width:33.33%;
+                    float:left;
+                    .button{
+                        margin:.1rem;
+                        padding:.1rem 0;
+                        text-align:center;
+                        border-radius:.06rem;
+                        border:.02rem solid #ccc
+                    }
+                }
+            }
+            .item-list{
+                .item{
+                    line-height:.76rem;
+                    padding-left:.2rem;
+                }
+            }
+        }
+    }
+</style>
